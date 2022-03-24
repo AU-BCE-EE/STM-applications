c1fc8f5dba26d187756f87de2db77e29a72fb100
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-03-23 21:22:56 -0400

    Add plots, run all

--- projects/2021_CH4_storage/model_eval/plot.R
+++ projects/2021_CH4_storage/model_eval/plot.R
@@ -2,6 +2,7 @@
 
 library(ggplot2)
 library(lubridate)
+library(reshape2)
 
 # Measurements
 dat <- read.csv('meas_temp/Sweden_storage_temp.csv')
@@ -24,8 +25,6 @@ dmt <- aggregate(temp ~ site + date, data = ddt, FUN = mean)
 
 dm <- rbind(dm, dmt)
 
-head(ddt, 2)
-head(dat, 2)
 dat <- rbind(dat, ddt)
 # NTS: name problems with height!
 
@@ -64,6 +63,17 @@ ggplot(dm, aes(doy, temp)) +
   theme(legend.position = 'top')
 ggsave('plots/ave_stor_temp_doy.png', height = 6, width = 8)
 
+ggplot(dm, aes(doy, temp)) +
+  geom_line(aes(doy, temp.floor), colour = 'gray32', lty = 1) +
+  geom_line(aes(doy, temp.air), colour = 'skyblue', lty = 1) +
+  geom_path(col = 'red') +
+  facet_wrap(~ site) +
+  labs(x = 'Day of year', y = expression('Temperature'~(degree*C)), 
+       colour = 'Position (from surface)') +
+  theme(legend.position = 'top')
+ggsave('plots/ave_stor_temp_doy_floor.png', height = 6, width = 8)
+
+
 ggplot(dm, aes(doy, depth.slurry)) +
   geom_line() +
   facet_wrap(~ site) +
@@ -71,3 +81,39 @@ ggplot(dm, aes(doy, depth.slurry)) +
        colour = 'Position (from surface)') +
   theme(legend.position = 'top')
 ggsave('plots/slurry_depth_doy.png', height = 6, width = 8)
+
+# Rates
+rates <- data.frame()
+ff <- list.files('stm_output', pattern = 'rates.txt')
+for (i in ff) {
+  d <- read.table(paste0('stm_output/', i), skip = 2, header = FALSE)
+  names(d) <- c('dos', 'doy', 'year', 'rad', 'air', 'floor', 'lwall',
+		'uwall', 'feed', 'tot', 'totave', 'hh', 'hhave', 'hadj', 
+		'hadjave', 'sst', 'ss')
+  d$site <- substr(i, 1, 4)
+  rates <- rbind(rates, d)
+}
+
+head(rates)
+rl <- melt(rates, id.vars = c('dos', 'doy', 'year', 'site'), 
+	   measure.vars = c('rad', 'air', 'floor', 'lwall', 'uwall', 'tot'))
+
+ggplot(rl, aes(doy, value, colour = variable)) +
+  geom_line() +
+  facet_wrap(~ site, scales = 'free') +
+  labs(x = 'Day of year', y = expression('Heat flow out'~(W/m^2))) +
+  theme(legend.position = 'top')
+ggsave('plots/heat_flow.png', height = 6, width = 8)
+
+
+rl2 <- melt(rates, id.vars = c('dos', 'doy', 'year', 'site', 'tot'), measure.vars = c('rad', 'air', 'floor', 'lwall', 'uwall'))
+rl2$qrel <- rl2$value / rl2$tot
+
+ggplot(rl2, aes(doy, qrel, colour = variable)) +
+  geom_line() +
+  facet_wrap(~ site, scales = 'free') +
+  labs(x = 'Day of year', y = expression('Heat flow out'~(W/m^2))) +
+  theme(legend.position = 'top')
+ggsave('plots/heat_flow_rel.png', height = 6, width = 8)
+
+
