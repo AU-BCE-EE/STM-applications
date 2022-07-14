11a467f31688751c73cbc32f6ee6fb335d9a676b
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-07-14 19:26:21 -0400

    Tweak legends

--- examples/02/plot.R
+++ examples/02/plot.R
@@ -7,7 +7,7 @@ ggplot(d, aes(date, temp.slurry, colour = factor(sim))) +
   geom_line(data = subset(dat, sim == 'Ref.'), aes(y = temp.air), colour = 'skyblue', alpha = 0.7, lty = 2) +
   geom_line() +
   labs(x = 'Date (month)', y = expression('Temperature'~(degree*C)), 
-       colour = 'Sim.:') +
+       colour = '') +
   theme_bw() +
   scale_color_brewer(palette = 'Dark2') +
   scale_x_date(date_breaks = "1 month", date_labels =  "%m") +
@@ -20,7 +20,7 @@ ggplot(d, aes(date, temp.slurry, colour = factor(sim))) +
   geom_line(aes(y = temp.air), alpha = 0.4, lty = '2121') +
   geom_line() +
   labs(x = 'Date (month)', y = expression('Temperature'~(degree*C)), 
-       colour = 'Sim.:') +
+       colour = '') +
   theme_bw() +
   scale_color_brewer(palette = 'Dark2') +
   scale_x_date(date_breaks = "1 month", date_labels =  "%m") +
@@ -31,7 +31,7 @@ ggsave('plots/ave_stor_temp_date_loc.pdf', height = 3.4, width = 3)
 ggplot(dat, aes(date, mass.slurry, colour = factor(sim))) +
   geom_line() +
   labs(x = 'Date (month)', y = expression('Slurry mass'~(kg)), 
-       colour = 'Sim.:') +
+       colour = '') +
   theme_bw() +
   scale_color_brewer(palette = 'Dark2') +
   scale_x_date(date_breaks = "1 month", date_labels =  "%m") +
@@ -40,13 +40,13 @@ ggsave('plots/slurry_mass_date.pdf', height = 3, width = 3)
 
 dd <- subset(dat, sim %in% c('Ref.', 'Emp.'))
 dd$sim <- as.character(dd$sim)
-dd$sim[dd$sim == 'Emp.'] <- 'All others'
-dd$sim <- factor(dd$sim, levels = c('Ref.', 'All others'))
+dd$sim[dd$sim == 'Ref.'] <- 'All others'
+dd$sim <- factor(dd$sim, levels = c('Emp.', 'All others'))
 ggplot(dd, aes(date, depth.slurry, colour = sim, lty = sim)) +
   geom_line() +
   scale_color_brewer(palette = 'Dark2') +
   labs(x = 'Date (month)', y = 'Slurry depth (m)', 
-       colour = 'Sim.:', lty = 'Sim.:') +
+       colour = '', lty = '') +
   scale_x_date(date_breaks = "1 month", date_labels =  "%m") +
   theme_bw() +
   theme(legend.position = 'top')
