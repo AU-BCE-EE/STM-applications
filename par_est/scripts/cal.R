b58906afe85082aad3809b96e169d8394d905d17
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-09-06 15:03:34 -0400

    par v0.3.
    Includes fixed R_air.

--- par_est/scripts/cal.R
+++ par_est/scripts/cal.R
@@ -1,8 +1,8 @@
 # Parameter estimation
 
 # Initial par guesses
-fixed <- c(heatGen = 0, Rconc = 0.3)
-p <- c(Rair = 0.02, Rslur = 0.7, Rsoil = 1.0, absorp = 0.02, soilDamp = 3)
+fixed <- c(heatGen = 0, Rconc = 0.3, Rair = 0.02)
+p <- c(Rslur = 0.7, Rsoil = 1.0, absorp = 0.02, soilDamp = 3)
 
 # Finally, parameter estimation
 m <- optim(par = p, fn = function(par) resCalc(p = par, meas = meas, fixed = fixed), method = 'Nelder-Mead')
