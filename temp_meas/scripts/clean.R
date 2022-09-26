9b777df49405ba463ac2edafbac1b34493ae01ae
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-09-26 18:30:57 -0400

    Switch to anonymous A, B, C... names

--- temp_meas/scripts/clean.R
+++ temp_meas/scripts/clean.R
@@ -7,10 +7,6 @@ dat <- rbind(dat.se, dat.dk)
 dat$date.time <- ymd_hm(dat$date.time)
 dat$date <- date(dat$date.time)
 
-# Site names
-dat$site.short <- substr(dat$site, 1, 4)
-dat$site.short[dat$site.short == 'Rånä'] <- 'Raan'
-
 # Save all depths for plotting
 dat.all <- dat
 
@@ -22,10 +18,10 @@ dat[, depth.max := max(depth), site]
 dat <- subset(dat, depth == depth.min | depth == depth.max)
 
 # Average measured temperature
-dat.mean <- aggregate(temp ~ site + site.short + date, data = dat, FUN = mean)
+dat.mean <- aggregate(temp ~ site + date, data = dat, FUN = mean)
 
 # Get unique depths used for reporting
 depths <- unique(dat[, c('site', 'depth.min', 'depth.max')])
 
 # Get date ranges
-dates <- aggregate(date ~ site + site.short, data = dat, FUN = function(x) as.character(range(x)))
+dates <- aggregate(date ~ site, data = dat, FUN = function(x) as.character(range(x)))
