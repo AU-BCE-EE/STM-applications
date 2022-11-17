9fabace4eafc9d61f6ada80d3980c0c0b67296c9
Sasha D. Hafner <sasha@hafnerconsulting.com>
2022-11-17 10:13:50 -0500

    Drop faulty 4 first site E measurement Tjele

--- examples/03/R_scripts/merge.R
+++ examples/03/R_scripts/merge.R
@@ -10,3 +10,6 @@ dl <- melt(dat, id.vars = c('site', 'date', 'doy', 'year'),
 
 dl$variable <- factor(dl$variable, levels = c('air_temp', 'temp', 'slurry_temp'), 
 		      labels = c('Measured air', 'Measured slurry', 'STM slurry'))
+
+# Drop faulty early 4 points from site E
+dl <- subset(dl, date > as.Date('2020-09-25') | site != 'E')
