33dd4a6d53f924cba28e8dd4c155ef652d1d0116
Sasha D. Hafner <sasha@hafnerconsulting.com>
2022-12-06 12:33:37 -0500

    Dashed lines for STM

--- examples/06/plot.R
+++ examples/06/plot.R
@@ -16,6 +16,7 @@ c10$variable <- 'STM 10C'
 c25$variable <- 'STM 25C'
 
 mod <- rbind(nh, c10, c25)
+mod$Type <- 'STM'
 
 mod$year <- mod$year + 2012
 mod$date <- as.POSIXct(paste(mod$year, mod$doy), format = '%Y %j')
@@ -23,9 +24,12 @@ mod$date <- as.POSIXct(paste(mod$year, mod$doy), format = '%Y %j')
 mod <- subset(mod, date >= min(meas$date) & date <= max(meas$date))
 
 meas$variable = 'Measured slurry'
+meas$Type <- 'Measured'
+
+head(meas)
 
 mod$value <- mod$slurry_temp
-dl <- rbind(meas[, c('date', 'doy', 'variable', 'value')], mod[, c('date', 'doy', 'variable', 'value')]) 
+dl <- rbind(meas[, c('date', 'doy', 'variable', 'value', 'Type')], mod[, c('date', 'doy', 'variable', 'value', 'Type')]) 
 #mod$variable = 'Air'
 #mod$value <- mod$air_temp
 #dl <- rbind(dl, mod[, c('date', 'doy', 'variable', 'value')]) 
@@ -33,25 +37,25 @@ dl <- rbind(meas[, c('date', 'doy', 'variable', 'value')], mod[, c('date', 'doy'
 dl$variable <- factor(dl$variable, levels = c('Measured slurry', 'STM no heat', 'STM 10C', 'STM 25C'))
 
 ggplot(dl, aes(doy, value, colour = variable)) +
-  geom_line() +
-  labs(x = 'Day of year', y = expression('Temperature'~(degree*C)), 
-       colour = '') +
+  geom_line(aes(lty = Type)) +
+  labs(x = 'Day of year', y = expression('Slurry temperature'~(degree*C)), 
+       colour = '', lty = '') +
   theme_bw() +
   scale_color_brewer(palette = 'Dark2') +
   theme(legend.position = 'right')
 ggsave('plots/ex6_ave_stor_temp_doy.pdf', height = 3, width = 5)
 
 ggplot(dl, aes(date, value, colour = variable)) +
-  geom_line() +
-  labs(x = 'Date', y = expression('Temperature'~(degree*C)), 
-       colour = '') +
+  geom_line(aes(lty = Type)) +
+  labs(x = 'Date', y = expression('Slutty temperature'~(degree*C)), 
+       colour = '', lty = '') +
   theme_bw() +
   scale_color_brewer(palette = 'Dark2') +
   theme(legend.position = 'right')
 ggsave('plots/ex6_ave_stor_temp_date.pdf', height = 3, width = 5)
 
 ggplot(mod, aes(date, slurry_depth)) +
-  geom_line() +
+  geom_line(aes(lty = Type)) +
   labs(x = 'Date of year', y = 'Level (m)') +
   theme_bw() +
   scale_color_brewer(palette = 'Dark2') +
