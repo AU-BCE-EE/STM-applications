bf1b4f88ef36e4eacf934e2efb663ed5c1b019f4
Sasha D. Hafner <sasha@hafnerconsulting.com>
2022-12-06 09:26:50 -0500

    Add Ottawa

---
+++ examples/06/run.sh
@@ -0,0 +1,8 @@
+# Runs simulations and makes plots
+
+# Remove old results
+
+# Run all simulations in parallel
+./stm noheat pars.txt user_pars_noheat.txt weather.csv level.csv
+./stm constant10 pars.txt user_pars_constant10.txt weather.csv level.csv
+./stm constant25 pars.txt user_pars_constant25.txt weather.csv level.csv
