a7e91ad37bbe8e419efcc0ec967f63a61718f436
Sasha D. Hafner <sasha@hafnerconsulting.com>
2022-12-12 12:43:50 -0500

    Clean up

--- par_sets/copy_pars.sh
+++
@@ -1,6 +0,0 @@
-# Copies latest set of parameters to examples directories
-
-cp pars.txt ../examples/01/pars.txt
-cp pars.txt ../examples/02/pars.txt
-cp pars.txt ../examples/03/pars/pars.txt
-cp pars.txt ../examples/04/pars/pars.txt
