74cf7f2b153e2e072f3026bfab441f4a470fdc72
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-02-06 10:14:23 -0500

    Try new pars v0.10 w Ottawa, try 10C in instead of 25

--- examples/06/run.sh
+++ examples/06/run.sh
@@ -7,7 +7,7 @@ rm stm_output/*.*
 ./stm default pars/pars.txt pars/user_pars_noheat.txt weather/weather.csv level.csv &
 ./stm more_rad pars/pars_more_rad.txt pars/user_pars_noheat.txt weather/weather.csv level.csv &
 ./stm heat_gen pars/pars_heat_gen.txt pars/user_pars_noheat.txt weather/weather.csv level.csv &
-./stm constant25 pars/pars.txt pars/user_pars_constant25.txt weather/weather.csv level.csv 
+./stm constant10 pars/pars.txt pars/user_pars_constant10.txt weather/weather.csv level.csv 
 
 # Move output
 mv *_temp.csv* stm_output
