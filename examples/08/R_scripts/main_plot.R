b3c8e82972fd5ae80a398fc228c585ad44f39bf8
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-02-28 15:33:36 -0500

    mv example 8 to STM-and-1D-model repo

--- examples/08/R_scripts/main_plot.R
+++
@@ -1,9 +0,0 @@
-# Plots measured and calculated temperatures
-
-rm(list = ls())
-
-source('packages.R')
-source('load.R')
-source('merge.R')
-source('plot.R')
-
--- examples/08/R_scripts/packages.R
+++
@@ -1,7 +0,0 @@
-
-library(ggplot2)
-library(lubridate)
-library(reshape2)
-library(viridis)
-library(data.table)
-
